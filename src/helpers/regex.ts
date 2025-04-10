7f7ffb157d1a56afee54349daa7c99f19ccde20c
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-04-11 05:16:14 +0700

    Add regex patterns for name and email validation
    - Introduced `NAME_REGEXP` for validating names against specific character rules.
    - Added `EMAIL_REGEXP` to ensure proper email format compliance, enhancing input validation across the application.

---
+++ src/helpers/regex.ts
@@ -0,0 +1,2 @@
+export const NAME_REGEXP = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/
+export const EMAIL_REGEXP = /^([0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([0-9a-zA-Z]+[-._])*[0-9a-zA-Z]+\.[a-zA-Z]{2,3}$/
