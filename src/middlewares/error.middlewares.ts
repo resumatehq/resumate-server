76ff132d04b9d400371c755a3f4120d28abd4353
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-04-25 20:11:10 +0700

    feat: Implement custom logging functionality with Winston
    - Added MyLogger class for structured logging with support for info, error, warn, and debug levels.
    - Configured Winston with daily rotating file transports for both info and error logs.
    - Integrated logging into the default error handler middleware to capture error details and request context for better debugging and traceability.

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -1,10 +1,32 @@
-import { Request, Response, NextFunction, ErrorRequestHandler } from 'express'
+import { ErrorRequestHandler } from 'express'
 import { omit } from 'lodash'
 import HTTP_STATUS_CODES from '~/core/statusCodes'
+import { logger } from '~/loggers/my-logger.log'
 import { ErrorWithStatus } from '~/utils/error.utils'
 
 // Định nghĩa rõ kiểu là ErrorRequestHandler
 export const defaultErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  const requestInfo = {
+    method: req.method,
+    originalUrl: req.originalUrl,
+    params: req.params,
+    body: omit(req.body, ['password', 'confirmPassword']), // Loại bỏ các field nhạy cảm
+    query: req.query,
+    userIP: req.ip || req.socket.remoteAddress
+  }
+
+  logger.error(
+    err.message,
+    'ErrorHandler', // Tên của service/context
+    Array.isArray(req.headers['x-request-id']) ? req.headers['x-request-id'][0] : req.headers['x-request-id'] || 'NO_REQUEST_ID', // requestId
+    {
+      errorName: err.name,
+      errorStack: err.stack,
+      status: err instanceof ErrorWithStatus ? err.status : HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR,
+      requestInfo
+    }
+  )
+
   if (err instanceof ErrorWithStatus) {
     res.status(err.status).json(omit(err, 'status'))
     return
