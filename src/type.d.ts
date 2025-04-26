03ba61c8af54d92afa218742bfead535e77f9935
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-04-26 16:24:15 +0700

    feat: Extend Express Request interface to include user information
    - Added a new optional property 'user' of type IUser to the Express Request interface in type.d.ts, allowing for better integration of user data within request handling.

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,8 +1,10 @@
 import { TokenPayload } from "./models/requests/user.request";
+import { IUser } from "./models/schemas/user.schema";
 declare global {
   namespace Express {
     interface Request {
-      decoded_authorization?: TokenPayload
+      decoded_authorization?: TokenPayload,
+      user?: IUser;
     }
   }
 }
