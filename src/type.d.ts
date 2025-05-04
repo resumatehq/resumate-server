6648c0039e4ccc7c08c911caed79fab4f24cc374
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-05-04 19:51:20 +0700

    feat: Add resume property to Express Request interface
    - Introduced a new optional property `resume` of type `IResume` to the Express Request interface for enhanced user data handling.

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,10 +1,12 @@
 import { TokenPayload } from "./models/requests/user.request";
+import { IResume } from "./models/schemas/resume.schema";
 import { IUser } from "./models/schemas/user.schema";
 declare global {
   namespace Express {
     interface Request {
       decoded_authorization?: TokenPayload,
       user?: IUser;
+      resume?: IResume
     }
   }
 }
